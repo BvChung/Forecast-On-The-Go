a9eef464a3053e9e9841bf70e81491ce6ba68389
Brandon Chung <chung.brandon123@gmail.com>
2022-01-19 00:02:45 -0600

    Adjusting async functions and storing values from API

--- model.js
+++ model.js
@@ -1,18 +1,32 @@
+// Stores Data from API
 export const state = {
-	coordinates: {},
+	coord: {},
 	forecast: [],
 };
 
 export const getCoordinates = async function (cityName, unitType = "metric") {
-	const res = await fetch(
-		`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${unitType}&appid=7b5dd67340ea04d52831fd0aa8f18630`
-	);
-	console.log(res);
+	try {
+		const res = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${unitType}&appid=7b5dd67340ea04d52831fd0aa8f18630`
+		);
+		console.log(res);
 
-	const data = await res.json();
-	console.log(data);
+		const data = await res.json();
+		console.log(data);
 
-	const { coord } = data;
+		const { coord } = data;
+		state.coord = {
+			latitude: coord.lat,
+			longitude: coord.lon,
+		};
+	} catch (err) {
+		console.error(err);
+	}
 };
 
-export const weather = async function () {};
+export const weather = async function () {
+	try {
+	} catch (err) {
+		console.error(err);
+	}
+};
--- script.js
+++ script.js
@@ -1,46 +1,13 @@
 // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+import * as model from "./model.js";
 
 //unitType = metric or imperial (c/f)
 
-let longitude, latitude;
-
-
-const weather = async function (cityName, unitType = "metric") {
-	try{
-	const res = await fetch(
-		`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${unitType}&appid=`
-	);
-	console.log(res);
-
-	const data = await res.json();
-	console.log(data);
-
-	const { coord } = data;
-	console.log(coord);
-	// const { temp: currentTemp, temp_max: maxTemp, temp_min: minTemp } = data.main;
-	// const { feels_like, humidity, pressure } = data.main;
-	// const { description } = data.weather[0];
-	// const { speed: windSpeed } = data.wind;
-
-	// console.log(
-	// 	lat,
-	// 	lon,
-	// 	currentTemp,
-	// 	currentTemp,
-	// 	maxTemp,
-	// 	minTemp,
-	// 	feels_like,
-	// 	humidity,
-	// 	pressure,
-	// 	description,
-	// 	windSpeed
-	// );
-	} catch(err) {
-		console.error(err);
-	}
-	};
-
-weather("houston", `imperial`);
+const displayWeather = async function () {
+	await model.getCoordinates("Houston", "metric");
+	console.log(model.state.coord);
+};
+displayWeather();
 
 // Unix converter will get unix sunset time to determine what day it is sunday 0 -> forward
 const unixConverter = async function (unix) {
@@ -52,9 +19,9 @@ const unixConverter = async function (unix) {
 	const day = date.getDay();
 	console.log(day);
 };
-unixConverter(1642425426);
-unixConverter(1642598200);
-unixConverter(1642511814);
+// unixConverter(1642425426);
+// unixConverter(1642598200);
+// unixConverter(1642511814);
 
 const sat = new Date(`January 22, 2022`);
 console.log(sat.getDay());
