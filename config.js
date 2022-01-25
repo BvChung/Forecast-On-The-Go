f713edc614926ef837ff02e6b1f5fe0052091e60
Brandon Chung <chung.brandon123@gmail.com>
2022-01-25 04:41:01 -0600

    Functionality change

--- config.js
+++ config.js
@@ -1,1 +1,57 @@
 export const API_KEY = `7b5dd67340ea04d52831fd0aa8f18630`;
+
+export const forecastIcon = function (icon) {
+	if (icon === "01d" || icon === "01n") {
+		return `<ion-icon class="weather-icon-forecast" name="sunny-outline"></ion-icon>`;
+	}
+	if (icon === "02d") {
+		return `<ion-icon class="weather-icon-forecast" name="partly-sunny-outline"></ion-icon>`;
+	}
+	if (icon === "02n") {
+		return `<ion-icon class="weather-icon-forecast" name="cloudy-night-outline"></ion-icon>`;
+	}
+	if (icon === "03d" || icon === "03n" || icon === "04d" || icon === "04n") {
+		return `<ion-icon class="weather-icon-forecast
+		" name="cloud-outline"></ion-icon>`;
+	}
+	if (icon === "09d" || icon === "09n" || icon === "10d" || icon === "10n") {
+		return `<ion-icon class="weather-icon-forecast" name="rainy-outline"></ion-icon>`;
+	}
+	if (icon === "11d" || icon === "11n") {
+		return `<ion-icon class="weather-icon-forecast" name="thunderstorm-outline"></ion-icon>`;
+	}
+	if (icon === "13d" || icon === "13n") {
+		return `<ion-icon class="weather-icon-forecast" name="snow-outline"></ion-icon>`;
+	}
+	if (icon === "50d" || icon === "50n") {
+		return `<ion-icon class="weather-icon-forecast" name="barcode-outline"></ion-icon>`;
+	}
+};
+
+export const weatherInfoIcon = function (icon) {
+	if (icon === "01d" || icon === "01n") {
+		return `<ion-icon class="weather-icon-current" name="sunny-outline"></ion-icon>`;
+	}
+	if (icon === "02d") {
+		return `<ion-icon class="weather-icon-current" name="partly-sunny-outline"></ion-icon>`;
+	}
+	if (icon === "02n") {
+		return `<ion-icon class="weather-icon-current" name="cloudy-night-outline"></ion-icon>`;
+	}
+	if (icon === "03d" || icon === "03n" || icon === "04d" || icon === "04n") {
+		return `<ion-icon class="weather-icon-current
+		" name="cloud-outline"></ion-icon>`;
+	}
+	if (icon === "09d" || icon === "09n" || icon === "10d" || icon === "10n") {
+		return `<ion-icon class="weather-icon-current" name="rainy-outline"></ion-icon>`;
+	}
+	if (icon === "11d" || icon === "11n") {
+		return `<ion-icon class="weather-icon-current" name="thunderstorm-outline"></ion-icon>`;
+	}
+	if (icon === "13d" || icon === "13n") {
+		return `<ion-icon class="weather-icon-current" name="snow-outline"></ion-icon>`;
+	}
+	if (icon === "50d" || icon === "50n") {
+		return `<ion-icon class="weather-icon-current" name="barcode-outline"></ion-icon>`;
+	}
+};
---
+++ forecastView.js
@@ -0,0 +1,44 @@
+import { weeklyForecast } from "./model.js";
+import WeatherView from "./view.js";
+
+class ForecastDisplay extends WeatherView {
+	_parentElement = document.querySelector(".weather-forecast-container");
+	_forecastParent = document.querySelector(".day--container");
+	_forecast;
+
+	renderWeekly(dataWeekly, units) {
+		this._forecast = dataWeekly;
+		console.log(this._forecast, units);
+
+		const markup = this.generateForecastMarkup();
+		// console.log(markup);
+		this._clearHTML();
+		this._parentElement.insertAdjacentHTML("afterbegin", markup);
+		// this.insertForecastIcon(this._dataWeekly[0].weatherIcon);
+	}
+
+	forecastIcon() {}
+
+	generateForecastMarkup() {
+		return this._forecast.map(this.generateMarkupPreview).join("");
+	}
+
+	generateMarkupPreview(result) {
+		return `<div class="day--container" id='color'>
+        <div class="day--title">
+            <p class="week-title">${result.weekday}</p>
+        </div>
+
+        <div class="forecast-icon">
+            ${result.forecastIcon}
+        </div>
+
+        <div class="day--temp">
+            <span class="week-temp-high">${result.dayTemperature}°</span>
+            <span class="week-temp-low">${result.nightTemperature}°</span>
+        </div>
+    </div>`;
+	}
+}
+
+export default new ForecastDisplay();
