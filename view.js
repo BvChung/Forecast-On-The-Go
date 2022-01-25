bd5c0be3a0d47947ec7e79326bf9a1e96d17cfdd
Brandon Chung <chung.brandon123@gmail.com>
2022-01-25 04:41:23 -0600

    Functionality change

--- view.js
+++ view.js
@@ -1,16 +1,24 @@
-export default class WeatherApp {
-	render(weeklyForecast, dailyForecast, units) {
+export default class WeatherView {
+	_dataCurrent;
+	_dataWeekly;
+	_units;
+	_currentIconContainer = document.querySelector(".info-logo");
+
+	render(dataCurrent, dataWeekly, units) {
 		//   Want to check if there is no data or if the data is an empty array
 		// if (!data || (Array.isArray(data) && data.length === 0))
 		//   return this.renderError();
 
 		// this._data is equal to the recipe data from the api
-		// console.log(this.city);
+		this._dataCurrent = dataCurrent;
+		this._dataWeekly = dataWeekly;
+		this._units = units;
+		console.log(this._dataCurrent, this._dataWeekly, this._units);
 
-		const markup = this.generateMarkup(weeklyForecast, dailyForecast, units);
+		const markup = this.generateMarkup();
 		this._clearHTML();
 		this._parentElement.insertAdjacentHTML("afterbegin", markup);
-		this.insertWeatherIcon(weeklyForecast[0].weatherIcon);
+		// this._currentIconContainer.innerHTML = this._dataWeekly[0].icon;
 	}
 
 	addHandlerSearch(handler) {
@@ -21,46 +29,11 @@ export default class WeatherApp {
 		});
 	}
 
-	insertWeatherIcon(icon) {
-		if (icon === "01d" || icon === "01n") {
-			document.querySelector(
-				".info-logo"
-			).innerHTML = `<ion-icon class="weather-icon" name="sunny-outline"></ion-icon>`;
-		}
-		if (icon === "02d") {
-			document.querySelector(
-				".info-logo"
-			).innerHTML = `<ion-icon class="weather-icon" name="partly-sunny-outline"></ion-icon>`;
-		}
-		if (icon === "02n") {
-			document.querySelector(
-				".info-logo"
-			).innerHTML = `<ion-icon class="weather-icon" name="cloudy-night-outline"></ion-icon>`;
-		}
-		if (icon === "03d" || icon === "03n" || icon === "04d" || icon === "04n") {
-			document.querySelector(
-				".info-logo"
-			).innerHTML = `<ion-icon class="weather-icon" name="cloud-outline"></ion-icon>`;
-		}
-		if (icon === "09d" || icon === "09n" || icon === "10d" || icon === "10n") {
-			document.querySelector(
-				".info-logo"
-			).innerHTML = `<ion-icon class="weather-icon" name="rainy-outline"></ion-icon>`;
-		}
-		if (icon === "11d" || icon === "11n") {
-			document.querySelector(
-				".info-logo"
-			).innerHTML = `<ion-icon class="weather-icon" name="thunderstorm-outline"></ion-icon>`;
-		}
-		if (icon === "13d" || icon === "13n") {
-			document.querySelector(
-				".info-logo"
-			).innerHTML = `<ion-icon class="weather-icon" name="snow-outline"></ion-icon>`;
-		}
-		if (icon === "50d" || icon === "50n") {
-			document.querySelector(
-				".info-logo"
-			).innerHTML = `<ion-icon class="weather-icon" name="barcode-outline"></ion-icon>`;
-		}
+	_clear() {
+		this._inputParentEl.querySelector(".input-text-search").value = "";
+	}
+
+	_clearHTML() {
+		this._parentElement.innerHTML = "";
 	}
 }
