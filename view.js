b590a49aaac20725557270a174d153a3ee9b86dd
Brandon Chung <chung.brandon123@gmail.com>
2022-01-24 19:03:54 -0600

    Style changes

--- view.js
+++ view.js
@@ -1,63 +1,18 @@
-class WeatherAppDisplay {
-	_inputParentEl = document.querySelector(".info-input");
-	_date = document.querySelector(".city-date");
+export default class WeatherApp {
 	_celcius = document.querySelector(".btn--cel");
 	_farenheit = document.querySelector(".btn--faren");
 	_currentUnits = true; //true = celcius and false = farenheit
 
-	city;
+	render(rainInfo, otherInfo) {
+		//   Want to check if there is no data or if the data is an empty array
+		// if (!data || (Array.isArray(data) && data.length === 0))
+		//   return this.renderError();
 
-	getLocation() {
-		const city = this._inputParentEl.querySelector(".input-text-search").value;
-		this._clear();
-		this.city = city;
-		return city;
-	}
+		// this._data is equal to the recipe data from the api
+		// console.log(this.city);
 
-	addHandlerSearch(handler) {
-		// Add to parent element so that event occurs when enter is pressed or button is clicked
-		this._inputParentEl.addEventListener("submit", function (e) {
-			e.preventDefault();
-			handler();
-		});
-	}
-
-	_clear() {
-		this._inputParentEl.querySelector(".input-text-search").value = "";
-	}
-
-	displayWeatherCity() {
-		const markup = `
-        <div class="info-title">Scattered Clouds</div>
-
-					<div class="info-city">
-						<p>Austin</p>
-					</div>
-
-					<div class="info-date">
-						<p>Tuesday, 18th Jan '22</p>
-						<p>4:12 pm</p>
-					</div>
-					<div class="info-temp">
-						<p>25 C</p>
-					</div>
-        `;
-	}
-
-	displayDate() {
-		const currentDate = new Date();
-		const dateOptions = {
-			day: "numeric",
-			month: "long",
-			year: "numeric",
-			weekday: "long",
-		};
-		const locale = navigator.language;
-		this._date.textContent = new Intl.DateTimeFormat(
-			locale,
-			dateOptions
-		).format(currentDate);
+		const markup = this.generateMarkup(rainInfo, otherInfo);
+		this._clearHTML();
+		this._parentElement.insertAdjacentHTML("afterbegin", markup);
 	}
 }
-
-export default new WeatherAppDisplay();
