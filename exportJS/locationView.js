e8b1c92d5e90cf706d4b8e1760507cda9150970d
Brandon Chung <chung.brandon123@gmail.com>
2022-02-17 14:45:28 -0600

    Css changes

--- exportJS/locationView.js
+++ exportJS/locationView.js
@@ -33,88 +33,85 @@ class WeatherSearch extends WeatherView {
 	generateMarkup() {
 		return `
 		<div class="weather-info--details">
-							<div class="info-title">
-							<div class="info-temp">
-							<p class="temp">${this._dataCurrent.currentTemp} ${
+			<div class="info-title">
+				<div class="info-temp">
+					<p class="temp">${this._dataCurrent.currentTemp} ${
 			this._units === "metric" ? "°C" : "°F"
 		}</p>
-						</div>
-								<div class="info--weather">
-									
-									<h1 class="info-header header-primary">${
-										this._dataWeekly[0].weather[0].toUpperCase() +
-										this._dataWeekly[0].weather.slice(1)
-									}</h1>
-									
-									<div class="info-logo">
-									${this._dataWeekly[0].infoIcon}
-									</div>
-									
-								</div>
-								
+				</div>
+
+				<div class="info--weather">
+					
+					<h1 class="info-header header-primary">${
+						this._dataWeekly[0].weather[0].toUpperCase() +
+						this._dataWeekly[0].weather.slice(1)
+					}</h1>
+					
+					<div class="info-logo">
+					${this._dataWeekly[0].infoIcon}
+					</div>
+					
+				</div>
 	
-								
-								
-	
-								
-							</div>
-							<div class="info-details">
-							<div class="info-current">
-								<p class="info--title">Precipitation</p>
-								<div class="attribute">
-									<ion-icon
-									name="rainy-outline"
-									class="info-icon"
-									></ion-icon
-									>
-								<span>${(this._dataWeekly[0].chanceOfRain * 100).toFixed(0)} %</span>
-							</div>
+			</div>
+
+		<div class="info-details">
+			<div class="info-current">
+					<p class="info--title">Precipitation</p>
+				<div class="attribute">
+					<ion-icon
+					name="rainy-outline"
+					class="info-icon"
+					></ion-icon
+					>
+				<span>${(this._dataWeekly[0].chanceOfRain * 100).toFixed(0)} %</span>
+			</div>
+		</div>
+
+		<div class="info-current">
+			<p class="info--title">Humidity</span>
+			<div class="attribute">
+				<ion-icon
+				name="water-outline"
+				class="info-icon"
+				></ion-icon
+				>
+			<span>${this._dataCurrent.humidity} %</span>
+		</div>
+
+		</div>
+			<div class="info-current">
+				<p class="info--title">Wind Speed</p>
+			<div class="attribute">
+				<ion-icon
+				name="shuffle-outline"
+				class="info-icon"
+				></ion-icon
+				>
+			<span>
+				${
+					this._units === "metric"
+						? `${Math.round(this._dataWeekly[0].wind * 3.6)} km/h`
+						: `${Math.round(this._dataWeekly[0].wind)} mph`
+				}</span>
 						</div>
 
-						<div class="info-current">
-							<p class="info--title">Humidity</span>
-							<div class="attribute">
-								<ion-icon
-								name="water-outline"
-								class="info-icon"
-								></ion-icon
-								>
-							<span>${this._dataCurrent.humidity} %</span>
-						</div>
+			</div>
+		<div class="info-current">
+			<p class="info--title">Feels Like</p>
+			<div class="attribute">
+				<ion-icon
+					name="thermometer-outline"
+					class="info-icon"
+				></ion-icon
+				>
+				<span>
+				${this._dataCurrent.feels_like} ${this._units === "metric" ? "°C" : "°F"}</span>
+			</div>
+		</div>
+			</div>
 
-						</div>
-							<div class="info-current">
-								<p class="info--title">Wind Speed</p>
-							<div class="attribute">
-								<ion-icon
-								name="shuffle-outline"
-								class="info-icon"
-								></ion-icon
-								>
-							<span>
-								${
-									this._units === "metric"
-										? `${Math.round(this._dataWeekly[0].wind * 3.6)} km/h`
-										: `${Math.round(this._dataWeekly[0].wind)} mph`
-								}</span>
-										</div>
-
-							</div>
-						<div class="info-current">
-							<p class="info--title">Feels Like</p>
-							<div class="attribute">
-								<ion-icon
-									name="thermometer-outline"
-									class="info-icon"
-								></ion-icon
-								>
-								<span>
-								${this._dataCurrent.feels_like} ${this._units === "metric" ? "°C" : "°F"}</span>
-							</div>
-						</div>
-							</div>
-	
-						</div>`;
+		</div>`;
 	}
 
 	displayDate() {
